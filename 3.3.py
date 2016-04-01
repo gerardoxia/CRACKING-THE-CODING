a0855c8eaa34344e776efa6b271a262417ba1e07
gerardoxia <gerardo.xia@outlook.com>
2016-04-01 15:50:33 -0500

    3.3

--- 3.3.py
+++ 3.3.py
@@ -24,30 +24,30 @@ class Stack:
 
 class SetOfStacks:
 	def __init__(self,data):
-		self = Stack(data)
+		self.stack = Stack(data)
 		self.count = 1
 		self.index = 1
 	def pop(self):
-		if(self.top != None):
-			item = self.top.data
-			self.top = self.top.LLnext
+		if(self.stack.top != None):
+			item = self.stack.top.data
+			self.top = self.stack.top.LLnext
 		return item
 	def push(self,data):
 		if(self.count < 2):
 			temp = Node(data)
-			temp.LLnext = self.top
-			self.top = temp
+			temp.LLnext = self.stack.top
+			self.stack.top = temp
 			self.count=self.count+1
 		else:
 			temp=Stack(data)
 			self.index=self.index+1
-			temp.top.LLnext=self.top
-			self.top=temp.top
+			temp.top.LLnext=self.stack.top
+			self.stack=temp
 			self.count=1
 
 a=SetOfStacks(1)
-print(repr(a.top.data),"  count:  "+repr(a.count)+"  index: "+repr(a.index))
+print(repr(a.stack.top.data),"  count:  "+repr(a.count)+"  index: "+repr(a.index))
 a.push(2)
-print(repr(a.top.data),"  count:  "+repr(a.count)+"  index: "+repr(a.index))
+print(repr(a.stack.top.data),"  count:  "+repr(a.count)+"  index: "+repr(a.index))
 a.push(3)
-print(repr(a.top.data),"  count:  "+repr(a.count)+"  index: "+repr(a.index))
+print(repr(a.stack.top.data),"  count:  "+repr(a.count)+"  index: "+repr(a.index))
